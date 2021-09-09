f81f8c9b534b3e9c7bcb6b651925de7ec4ea14b8
David <id11011@usal.es>
2021-09-09 15:11:49 +0200

    Refactoring Calendar Grid

--- src/components/CalendarGrid.tsx
+++ src/components/CalendarGrid.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Table, Thead, Tbody, Th, Tr, Td, Grid, GridItem, useDisclosure, Modal, ModalBody, ModalContent, ModalCloseButton, ModalOverlay, ModalHeader, ModalFooter, Button } from "@chakra-ui/react";
 import { dayClicked, remindersMetaAdapter } from "../redux/reducers";
 import { Day as DayType, Reminder } from "../typings";
 import { Day } from "./Day";
 import { selectReminders, selectSelectedDate } from "../redux/selectors";
 import { CalendarUtils } from "../lib";
+//import ReminderItem from "./Reminder";
 
 type Props = { days: DayType[] };
 
 export const CalendarGrid: React.FC<Props> = ({ days }) => {
+
   const dispatch = useDispatch();
+
+  const [reminders, setReminders] = useState<any>()
+  
   const handleClick = (day: DayType) => {
     dispatch(dayClicked(day));
+    const date = CalendarUtils.createDateString(day);
+    if(remindersByDate[date] !== undefined) {
+      const reminders = remindersByDate[date]['entities']
+      const values = Object.values(reminders)
+      setReminders(values)
+      //Method
+      values.map(value => (
+          console.log(value?.text, value?.city, value?.hour)
+        )
+      )
+      //Necesito guardar esa data en algun sitio accesible desde la tabla
+      onOpen()
+    } else {
+      console.log("No reminders available")
+    }
+    
   };
 
   const selectedDate = useSelector(selectSelectedDate);
@@ -22,8 +43,49 @@ export const CalendarGrid: React.FC<Props> = ({ days }) => {
   const presentMonth = present.getMonth();
   const presentDay = present.getDate();
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <Grid templateColumns="repeat(7, 1fr)" gap={6}>
+      {/*Modal Start*/}
+      <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false} isCentered size={'3xl'}>
+        <ModalOverlay />
+        <ModalContent>
+        <ModalHeader>
+          Reminders
+        </ModalHeader>
+        <ModalCloseButton color="black"/>
+        <ModalBody fontSize="3xl" fontFamily="Times New Roman">
+        <Box mt={5} mb={10}>
+          <Table variant="striped" colorScheme="blackAlpha">
+            <Thead>
+              <Tr>
+                <Th textAlign="center">Text</Th>
+                <Th textAlign="center">City</Th>
+                <Th textAlign="center">Time</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+                {reminders ? reminders.map(reminder => (
+                <Tr key={reminder.id}>
+                  <Td textAlign="center">{reminder.text}</Td>
+                  <Td textAlign="center">{reminder.city}</Td>
+                  <Td textAlign="center">{reminder.hour}</Td>
+                  {/*<ReminderItem reminder={{}} />*/}
+                </Tr>
+                )) : null}
+            </Tbody>    
+          </Table>
+        </Box>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="gray" mr={3} onClick={onClose}>
+            Cerrar
+          </Button>
+        </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {/*Modal End*/}
       {days.map((day) => {
         const isPresentDay =
           day.day === presentDay &&
